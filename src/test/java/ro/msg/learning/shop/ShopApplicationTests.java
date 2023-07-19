cc7089b507b4106479e7fb2c97096362c413de7e
bogdan_ghetze@yahoo.com <bogdan_ghetze@yahoo.com>
2023-07-19 08:58:47 +0300

    Removed the commented test.

--- src/test/java/ro/msg/learning/shop/ShopApplicationTests.java
+++ src/test/java/ro/msg/learning/shop/ShopApplicationTests.java
@@ -1,12 +1,13 @@
 package ro.msg.learning.shop;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShopApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
